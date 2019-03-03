615916326f8d433f62057b5a93194c9cd64a060d
Floliroy <45274794+Floliroy@users.noreply.github.com>
2019-03-03 15:55:24 +0100

    Create fr.lua

---
+++ lang/fr.lua
@@ -0,0 +1,23 @@
+----------- ABILITIES -----------
+ZO_CreateStringId("SI_SYNERGY_ABILITY_CONDUIT", "Canalisation")
+ZO_CreateStringId("SI_SYNERGY_ABILITY_HARVEST", "Récolte")
+ZO_CreateStringId("SI_SYNERGY_ABILITY_CHARGED_LIGHTNING", "Éclair chargé")
+ZO_CreateStringId("SI_SYNERGY_ABILITY_IMPALE", "Empalement")
+ZO_CreateStringId("SI_SYNERGY_ABILITY_GRAVITY_CRUSH", "Anéantissement de gravité")
+ZO_CreateStringId("SI_SYNERGY_ABILITY_IMPALE", "Empalement")
+
+----------- TRIAL SYNERGIES -----------
+ZO_CreateStringId("SI_SYNERGY_ABILITY_DROP_HOARFROST", "Rejet de verglas") -- CR Hoarfrost
+ZO_CreateStringId("SI_SYNERGY_ABILITY_CELESTIAL_PURGE", "TODO") -- vmol something --maybe "Purge céleste" but not sure
+ZO_CreateStringId("SI_SYNERGY_ABILITY_POWER_SWITCH", "interrupteur") -- hof boss 2 switch
+ZO_CreateStringId("SI_SYNERGY_ABILITY_GATEWAY", "Portail") -- CR portal
+ZO_CreateStringId("SI_SYNERGY_ABILITY_REMOVE_BOLT", "Dissipation éclair") -- Ozara pin
+ZO_CreateStringId("SI_SYNERGY_ABILITY_DESTRUCTIVE_OUTBREAK", "Accès destructeur") -- Warrior break
+ZO_CreateStringId("SI_SYNERGY_ABILITY_MALEVOLENT_CORE", "Noyau malveillant") -- CR downstairs cores
+ZO_CreateStringId("SI_SYNERGY_ABILITY_WELKYNARS_LIGHT", "TODO") -- CR spears --maybe "Lumière de welkynar" but not sure
+ZO_CreateStringId("SI_SYNERGY_ABILITY_LEVITATE", "Lévitation") -- hof boss 2 pads
+
+----------- BOSSES -----------
+ZO_CreateStringId("SI_SYNERGY_BOSS_THE_MAGE", "La Mage")
+ZO_CreateStringId("SI_SYNERGY_BOSS_YOKEDA_KAI", "L'Yokeda Kai")
+ZO_CreateStringId("SI_SYNERGY_BOSS_YOKEDA_ROKDUN", "Yokeda Rok'dun")
