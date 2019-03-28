bcacdff9c31a5fb490ae07ebdf736b8fb00fe08d
Wheel5 <allentheskink16@gmail.com>
2019-03-28 14:35:00 -0400

    supportic changes (name formatting) + version update

--- Synergy.lua
+++ Synergy.lua
@@ -4,7 +4,7 @@ local EM = GetEventManager()
 local libDialog = LibStub('LibDialog')
 
 syn.name = "Synergy"
-syn.version = "1.11"
+syn.version = "1.12"
 
 local isMagDD
 
@@ -81,6 +81,7 @@ function syn.SynergyOverride()
 	function SYNERGY:OnSynergyAbilityChanged()
 		local n, _ = GetSynergyInfo()
 		local d, h, t = GetGroupMemberRoles('player')
+		n = zo_strformat("<<1>>", n) 
 		if n and syn.savedVariables.brpSynDisable and syn.blackrose[n] then return end
 		if n and syn.savedVariables.maSynDisable and syn.maelstrom[n] then return end
 		if n and syn.savedVariables.portalDisable and n == GetString(SI_SYNERGY_ABILITY_GATEWAY) then return end
